1ee4d893adf4e6b46968434e8048f45769489898
UPavankumar <u.pavankumar2002@gmail.com>
2026-08-19 15:00:13 +0530

    chore(home): set Home to a clean plain black page with navigation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,11 @@
 import { MotionConfig } from "framer-motion";
 import Nav from "../components/Nav";
-import LinearHero from "../components/LinearHome/LinearHero";
-import LinearConduitPipeline from "../components/LinearHome/LinearConduitPipeline";
-import LinearBentoGrid from "../components/LinearHome/LinearBentoGrid";
-import LinearMilestones from "../components/LinearHome/LinearMilestones";
-import LinearCTA from "../components/LinearHome/LinearCTA";
 
 export default function Home() {
   return (
     <MotionConfig reducedMotion="user">
       <Nav />
-      <main className="w-full min-h-screen bg-[#030712] text-white selection:bg-[#38bdf8]/30 selection:text-[#38bdf8] overflow-x-hidden">
-        <LinearHero />
-        <LinearConduitPipeline />
-        <LinearBentoGrid />
-        <LinearMilestones />
-        <LinearCTA />
-      </main>
+      <main className="w-full min-h-screen bg-black text-white" />
     </MotionConfig>
   );
 }
